302507dded78e212800848cce516a35b1cfd05ba
queiroz046 <gabrielqueiroz701@gmail.com>
2026-07-01 22:10:32 -0300

    Commit 3: Redireciona página inicial para o login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
